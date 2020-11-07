b9572e82e66bde75eb57525f7d88f7c4487036d2
8oki <tosh.tanaka.8fizz@gmail.com>
2020-11-07 23:32:17 +0900

    fix missing letter in the comment

--- app/localtime/japan.py
+++ app/localtime/japan.py
@@ -42,7 +42,7 @@ def get_japanese_weekday(weekday_index: int, long_format: bool = False) -> str:
 
 if __name__ == "__main__":
     jst_now = get_curret_jst()
-    print(jst_now)  # 020-11-07 11:16:38.959435+09:00
+    print(jst_now)  # 2020-11-07 11:16:38.959435+09:00
     print(jst_now.strftime(DATE_FORMAT))  # 2020年11月07日
     print(jst_now.strftime(TIME_FORMAT))  # 11時16分38秒
     print(get_japanese_weekday(jst_now.weekday()))  # 土
